48d29502151c0e7aac4a9eed61a9af16cefe01fd
Kenzo <kenzo.purnomo.stu@elyon.sch.id>
2026-04-10 12:53:57 +0700

    Add files via upload

---
+++ databaeforA350.php
@@ -0,0 +1,13 @@
+CREATE TABLE Userofa350(
+user VARCHAR(50),
+password VARCHAR(50)
+); 
+
+INSERT INTO Userofa350(user, password)VALUES 
+('Jayden', 'Everlynmysissy'),
+('William', 'leakingparentsnamesince25'),
+('Kenzo', 'Iluvcx'),
+('Elite', 'CXCEOSpeaking'),
+('Dennis', 'Formermod'),
+('Marc', 'depressedasHELL'),
+('Joshua', 'Bentkneepov');
